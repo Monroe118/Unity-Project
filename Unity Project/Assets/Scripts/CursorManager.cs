3caddc7935bae856f1cd1d9f8b2a9e7ce464069f
DESKTOP-FROHAJU\MoLiang <monroe@61bm.com>
2017-07-05 18:44:09 +0800

    重新创建HoloLens项目

---
+++ Unity Project/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,62 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+/// <summary>  
+/// CursorManager class takes Cursor GameObjects.  
+/// One that is on Holograms and another off Holograms.  
+/// 1. Shows the appropriate Cursor when a Hologram is hit.  
+/// 2. Places the appropriate Cursor at the hit position.  
+/// 3. Matches the Cursor normal to the hit surface.  
+/// </summary>  
+public partial class CursorManager : Singleton<CursorManager>
+{
+    //凝视射线在全息对象上时显示的光标对象  
+    [Tooltip("Drag the Cursor object to show when it hits a hologram.")]
+    public GameObject CursorOnHolograms;
+    //凝视射线离开全息对象时显示的光标对象  
+    [Tooltip("Drag the Cursor object to show when it does not hit a hologram.")]
+    public GameObject CursorOffHolograms;
+
+    [Tooltip("Distance, in meters, to offset the cursor from the collision point.")]
+    public float DistanceFromCollision = 0.01f;
+
+    void Awake()
+    {
+        //当未设定光标对象时直接返回  
+        if (CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+
+        // Hide the Cursors to begin with.  
+        CursorOnHolograms.SetActive(false);
+        CursorOffHolograms.SetActive(false);
+    }
+
+    void LateUpdate()
+    {
+        if (GazeManager.Instance == null || CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+        //当凝视射线在全息对象上及离开全息对象时，分别显示不同的光标对象，以此来进行区分  
+        if (GazeManager.Instance.HitObject)
+        {
+            CursorOnHolograms.SetActive(true);
+            CursorOffHolograms.SetActive(false);
+        }
+        else
+        {
+            CursorOffHolograms.SetActive(true);
+            CursorOnHolograms.SetActive(false);
+        }
+
+        //计算并安置光标  
+        // Place the cursor at the calculated position.  
+        this.gameObject.transform.position = GazeManager.Instance.HitPosition + GazeManager.Instance.GazeNormal * DistanceFromCollision;
+
+        // Orient the cursor to match the surface being gazed at.  
+        gameObject.transform.up = GazeManager.Instance.GazeNormal;
+    }
+}
---
+++ Unity Project/Assets/Scripts/GestureManager.cs
@@ -0,0 +1,127 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>  
+    /// GestureManager creates a gesture recognizer and signs up for a tap gesture.  
+    /// When a tap gesture is detected, GestureManager uses GazeManager to find the game object.  
+    /// GestureManager then sends a message to that game object.  
+    /// </summary>  
+    [RequireComponent(typeof(GazeManager))]
+    public partial class GestureManager : Singleton<GestureManager>
+    {
+        /// <summary>  
+        /// Key to press in the editor to select the currently gazed hologram  
+        /// </summary>  
+        public KeyCode EditorSelectKey = KeyCode.Space;
+
+        /// <summary>  
+        /// To select even when a hologram is not being gazed at,  
+        /// set the override focused object.  
+        /// If its null, then the gazed at object will be selected.  
+        /// </summary>  
+        public GameObject OverrideFocusedObject
+        {
+            get; set;
+        }
+
+        /// <summary>  
+        /// Gets the currently focused object, or null if none.  
+        /// </summary>  
+        public GameObject FocusedObject
+        {
+            get { return focusedObject; }
+        }
+
+        private GestureRecognizer gestureRecognizer;
+        private GameObject focusedObject;
+
+        public bool IsNavigating { get; private set; }
+        public Vector3 NavigationPosition { get; private set; }
+
+        void Start()
+        {
+            //  创建GestureRecognizer实例  
+            gestureRecognizer = new GestureRecognizer();
+            //  注册指定的手势类型,本例指定单击及双击手势类型  
+            gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap
+                | GestureSettings.DoubleTap);
+            //  订阅手势事件  
+            gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
+
+            //  开始手势识别  
+            gestureRecognizer.StartCapturingGestures();
+        }
+
+        private void OnTap()
+        {
+            if (focusedObject != null)
+            {
+                focusedObject.SendMessage("OnTap");
+            }
+        }
+
+        private void OnDoubleTap()
+        {
+            if (focusedObject != null)
+            {
+                focusedObject.SendMessage("OnDoubleTap");
+            }
+        }
+
+        private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+        {
+            if (tapCount == 1)
+            {
+                OnTap();
+            }
+            else
+            {
+                OnDoubleTap();
+            }
+        }
+
+        void LateUpdate()
+        {
+            GameObject oldFocusedObject = focusedObject;
+
+            if (GazeManager.Instance.HitObject &&
+                OverrideFocusedObject == null &&
+                GazeManager.Instance.HitInfo.collider != null)
+            {
+                // If gaze hits a hologram, set the focused object to that game object.  
+                // Also if the caller has not decided to override the focused object.  
+                focusedObject = GazeManager.Instance.HitInfo.collider.gameObject;
+            }
+            else
+            {
+                // If our gaze doesn't hit a hologram, set the focused object to null or override focused object.  
+                focusedObject = OverrideFocusedObject;
+            }
+
+            if (focusedObject != oldFocusedObject)
+            {
+                // If the currently focused object doesn't match the old focused object, cancel the current gesture.  
+                // Start looking for new gestures.  This is to prevent applying gestures from one hologram to another.  
+                gestureRecognizer.CancelGestures();
+                gestureRecognizer.StartCapturingGestures();
+            }
+
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(EditorSelectKey))
+            {
+                OnTap();
+            }
+#endif
+        }
+
+        void OnDestroy()
+        {
+            gestureRecognizer.StopCapturingGestures();
+            gestureRecognizer.TappedEvent -= GestureRecognizer_TappedEvent;
+        }
+    }
+}
